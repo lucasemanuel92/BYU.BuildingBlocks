5586e63fb5dcfc473fdfbcb54c0b037023b7289c
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2023-04-24 16:42:01 -0300

    Add files via upload

---
+++ week01/stringsFormat.py
@@ -0,0 +1,5 @@
+first_name = input("What is your first name?\n")
+last_name = input("What is your second name?\n")
+
+output = f"My name is {last_name}, {first_name} {last_name}".title()
+print(output)
