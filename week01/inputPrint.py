9322196d42ffd2779e9437de2b8e045cdc85d92d
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2023-04-24 16:41:43 -0300

    Add files via upload

---
+++ week01/inputPrint.py
@@ -0,0 +1,8 @@
+"""
+Author: Lucas Emanuel O. de Carvalho
+Purpose: Display text to the screen
+"""
+
+
+team = input('What is your favourite soccer team?\n')
+print('My favourite soccer team is ' + team)
