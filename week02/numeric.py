10a3f6a7307ca76e7cd8ebfe601165968ae3c61b
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2023-04-29 16:34:55 -0300

    Add files via upload

---
+++ week02/numeric.py
@@ -0,0 +1,15 @@
+age = int(input('How old are you?\n'))
+next_year = age + 1
+
+print(f'Your age next year will be {next_year}')
+
+eggs = int(input("\nHow many egg cartoons do you have?\n"))
+total_eggs = eggs * 12
+
+print(f"You have {total_eggs} in total")
+
+cookies = int(input("\nHow many cookies do you have?\n"))
+people = int(input("How many people are there?\n"))
+cookies_people = cookies / people
+
+print(f"\nEach person will get {cookies_people}, max.")
