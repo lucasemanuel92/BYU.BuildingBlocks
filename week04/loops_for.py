22c2d30d7618d2bdce6344fb9ec897aa90958f44
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2023-05-10 12:01:58 -0300

    Add files via upload

---
+++ week04/loops_for.py
@@ -0,0 +1,26 @@
+"""
+Author: Lucas Emanuel Oliveira de Carvalho
+Project 01: Create a loop for colors
+"""
+
+colors = ["red", "blue", "green", "yellow"]
+
+for color in colors:
+    print(color)
+
+print()
+
+"""
+Project 02: Create a loop to set numbers 1 - 8
+"""
+
+for index in range (1, 9):
+    print(index)
+
+print()
+"""
+Project 03: Create a loop for even numbers
+"""
+
+for index in range (2, 21, 2):
+    print(index)
