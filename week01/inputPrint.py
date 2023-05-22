5ef2e28b9a7bc0151799b5f4b73257c10a812511
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2023-05-22 16:06:46 -0300

    Delete inputPrint.py

--- week01/inputPrint.py
+++
@@ -1,9 +0,0 @@
-"""
-AUTHOR: Lucas Emanuel O. de Carvalho
-PPROJECT: write a program that uses both input (obtaining data from the user via the keyboard) 
-and output (displaying data to the user on the screen).
-"""
-
-
-team = input('What is your favourite soccer team?\n')
-print('My favourite soccer team is ' + team)
