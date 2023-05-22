bc69c4956974f66f77d46b48956cde4ee41a85b9
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2023-05-22 16:09:04 -0300

    Delete stringsFormat.py

--- week01/stringsFormat.py
+++
@@ -1,5 +0,0 @@
-first_name = input("What is your first name?\n")
-last_name = input("What is your second name?\n")
-
-output = f"My name is {last_name}, {first_name} {last_name}".title()
-print(output)
