e6b164d959c96654c9e0928e4ea3d6b4523ccc24
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2023-05-22 16:06:00 -0300

    Delete inputPrint.py

--- week01/inputPrint.py
+++
@@ -1,8 +0,0 @@
-"""
-Author: Lucas Emanuel O. de Carvalho
-Purpose: Display text to the screen
-"""
-
-
-team = input('What is your favourite soccer team?\n')
-print('My favourite soccer team is ' + team)
