7a4e2ca9f2d9c70020e16fa5f6f3bb6d6d81dc6d
Lucas Emanuel Oliveira de Carvalho <lucas.emanuel.carvalho@outlook.com>
2023-05-22 16:07:07 -0300

    Add files via upload

---
+++ week01/inputPrint.py
@@ -0,0 +1,9 @@
+"""
+AUTHOR: Lucas Emanuel O. de Carvalho
+PROJECT: write a program that uses both input (obtaining data from the user via the keyboard) 
+and output (displaying data to the user on the screen).
+"""
+
+
+team = input('What is your favourite soccer team?\n')
+print('My favourite soccer team is ' + team)
